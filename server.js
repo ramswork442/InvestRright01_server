e533516d7d62333ab8a5fddd71aca057754f5d6a
ramswork442 <ramswork442@gmail.com>
2025-03-02 19:16:06 +0530

    Update server.js
    update get api

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/events", eventRoutes);
 app.use('/api/courses', courseRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Invest Rright.... its's renderig!');
+   res.send('Hello from the InvestRright!');
 });
 
 const PORT = process.env.PORT || 5000;
